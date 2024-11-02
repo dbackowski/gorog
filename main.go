00fa4ab115c24f95ddbe6e2125f245e03a8b11a2
Damian Baćkowski <damianbackowski@gmail.com>
2024-11-02 16:53:42 +0100

    Refactor

--- main.go
+++ main.go
@@ -1,28 +1,109 @@
 package main
 
 import (
+	"image"
+	_ "image/png"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	Tiles []MapTile
+}
+
+type GameData struct {
+	ScreenWidth  int
+	ScreenHeight int
+	TileWidth    int
+	TileHeight   int
+}
+
+type MapTile struct {
+	PixelX  int
+	PixelY  int
+	Blocked bool
+	Image   *ebiten.Image
+}
 
 func NewGame() *Game {
 	g := &Game{}
+	g.Tiles = CreateTiles()
+
 	return g
 }
 
-// Update is called each tic.
+func NewGameData() GameData {
+	g := GameData{
+		ScreenWidth:  80,
+		ScreenHeight: 50,
+		TileWidth:    16,
+		TileHeight:   16,
+	}
+
+	return g
+}
+
+func GetIndexFromXY(x int, y int) int {
+	gd := NewGameData()
+	return (y * gd.ScreenWidth) + x
+}
+
+func CreateTiles() []MapTile {
+	gd := NewGameData()
+	tiles := make([]MapTile, 0)
+	img, _, err := ebitenutil.NewImageFromFile("assets/EverRogueTileset 1.0 Horizontal.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for x := 0; x < gd.ScreenWidth; x++ {
+		for y := 0; y < gd.ScreenHeight; y++ {
+			if x == 0 || x == gd.ScreenWidth-1 || y == 0 || y == gd.ScreenHeight-1 {
+				wall := img.SubImage(image.Rect(272, 0, 256, 272)).(*ebiten.Image)
+
+				tile := MapTile{
+					PixelX:  x * gd.TileWidth,
+					PixelY:  y * gd.TileHeight,
+					Blocked: true,
+					Image:   wall,
+				}
+				tiles = append(tiles, tile)
+			} else {
+				floor := img.SubImage(image.Rect(0, 0, 16, 16)).(*ebiten.Image)
+
+				tile := MapTile{
+					PixelX:  x * gd.TileWidth,
+					PixelY:  y * gd.TileHeight,
+					Blocked: false,
+					Image:   floor,
+				}
+				tiles = append(tiles, tile)
+			}
+		}
+	}
+
+	return tiles
+}
+
 func (g *Game) Update() error {
 	return nil
 }
 
-// Draw is called each draw cycle and is where we will blit.
 func (g *Game) Draw(screen *ebiten.Image) {
+	gd := NewGameData()
+
+	for x := 0; x < gd.ScreenWidth; x++ {
+		for y := 0; y < gd.ScreenHeight; y++ {
+			tile := g.Tiles[GetIndexFromXY(x, y)]
+			op := &ebiten.DrawImageOptions{}
+			op.GeoM.Translate(float64(tile.PixelX), float64(tile.PixelY))
+			screen.DrawImage(tile.Image, op)
+		}
+	}
 }
 
-// Layout will return the screen dimensions.
 func (g *Game) Layout(w, h int) (int, int) { return 1280, 800 }
 
 func main() {
