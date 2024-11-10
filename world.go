69bd61be93904eacbc400205f5848bc44f14d34a
Damian Baćkowski <damianbackowski@gmail.com>
2024-11-10 20:26:45 +0100

    Render player

--- world.go
+++ world.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"image"
+	"log"
+
 	"github.com/bytearena/ecs"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+var position *ecs.Component
+var renderable *ecs.Component
 
 func InitializeWorld() (*ecs.Manager, map[string]ecs.Tag) {
 	tags := make(map[string]ecs.Tag)
 	manager := ecs.NewManager()
 
+	img, _, err := ebitenutil.NewImageFromFile("assets/EverRogueTileset 1.0 Horizontal.png")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	playerImg := img.SubImage(image.Rect(2800, 0, 2784, 2800)).(*ebiten.Image) // player image
+
 	player := manager.NewComponent()
-	position := manager.NewComponent()
-	renderable := manager.NewComponent()
+	position = manager.NewComponent()
+	renderable = manager.NewComponent()
 	movable := manager.NewComponent()
 
 	manager.NewEntity().
-		AddComponent(player, Player{}).
-		AddComponent(renderable, Renderable{}).
+		AddComponent(renderable, &Renderable{
+			Image: playerImg,
+		}).
 		AddComponent(movable, Movable{}).
 		AddComponent(position, &Position{
 			X: 40,
@@ -24,6 +41,8 @@ func InitializeWorld() (*ecs.Manager, map[string]ecs.Tag) {
 
 	players := ecs.BuildTag(player, position)
 	tags["players"] = players
+	renderables := ecs.BuildTag(renderable, position)
+	tags["renderables"] = renderables
 
 	return manager, tags
 }
